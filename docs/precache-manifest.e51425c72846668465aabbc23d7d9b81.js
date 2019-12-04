12286aa7f39376a241d76fe0dc7a906278b9d01b
Jakob <jakob@folkdatorn.se>
2019-12-04 17:24:13 +0100

    Updated paths for gh-pages

---
+++ docs/precache-manifest.e51425c72846668465aabbc23d7d9b81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05432c1d00b69cdeb9bcb249e9e91a8f",
+    "url": "/react-tinder-card-demo/index.html"
+  },
+  {
+    "revision": "2f80d45dbc6a8729172e",
+    "url": "/react-tinder-card-demo/static/css/main.9f59e4e6.chunk.css"
+  },
+  {
+    "revision": "6755a0f71f4643c9fef1",
+    "url": "/react-tinder-card-demo/static/js/2.f2effef8.chunk.js"
+  },
+  {
+    "revision": "2f80d45dbc6a8729172e",
+    "url": "/react-tinder-card-demo/static/js/main.339656a4.chunk.js"
+  },
+  {
+    "revision": "0d6c761cd56427a1dae1",
+    "url": "/react-tinder-card-demo/static/js/runtime-main.aef4b6f8.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.053ba8d500728541f4aa74740279bb4b.js"
+  "/react-tinder-card-demo/precache-manifest.e51425c72846668465aabbc23d7d9b81.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tinder-card-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

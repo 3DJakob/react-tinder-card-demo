3df22c5aa63f168450af0e9f0992a7abc46dd214
Jakob <jakob@folkdatorn.se>
2020-10-07 18:57:06 +0200

    Added advanced example

--- src/App.js
+++ src/App.js
@@ -1,58 +1,20 @@
 import React, { useState } from 'react'
 import './App.css'
-import TinderCard from './react-tinder-card/index'
+import Switch from 'react-ios-switch'
 
-const db = [
-  {
-    name: 'Richard Hendricks',
-    url: './img/richard.jpg'
-  },
-  {
-    name: 'Erlich Bachman',
-    url: './img/erlich.jpg'
-  },
-  {
-    name: 'Monica Hall',
-    url: './img/monica.jpg'
-  },
-  {
-    name: 'Jared Dunn',
-    url: './img/jared.jpg'
-  },
-  {
-    name: 'Dinesh Chugtai',
-    url: './img/dinesh.jpg'
-  }
-]
+import Advanced from './Examples/Advanced'
+import Simple from './Examples/Simple'
 
 function App () {
-  const characters = db
-  const [lastDirection, setLastDirection] = useState()
-
-  const swiped = (direction, nameToDelete) => {
-    console.log('removing: ' + nameToDelete)
-    setLastDirection(direction)
-  }
-
-  const outOfFrame = (name) => {
-    console.log(name + ' left the screen!')
-  }
+  const [showAdvanced, setShowAdvanced] = useState(true)
+  console.log(showAdvanced)
 
   return (
-    <div>
-      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
-      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
-      <h1>React Tinder Card</h1>
-      <div className='cardContainer'>
-        {characters.map((character) =>
-          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
-            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
-              <h3>{character.name}</h3>
-            </div>
-          </TinderCard>
-        )}
+    <div className='app'>
+      {showAdvanced ? <Advanced /> : <Simple />}
+      <div className='row'>
+        <p style={{ color: '#fff' }}>Show advanced example</p> <Switch checked={showAdvanced} onChange={setShowAdvanced} />
       </div>
-      {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
     </div>
   )
 }
---
+++ src/Examples/Advanced.js
@@ -0,0 +1,81 @@
+import React, { useState, useMemo } from 'react'
+import TinderCard from '../react-tinder-card/index'
+
+const db = [
+  {
+    name: 'Richard Hendricks',
+    url: './img/richard.jpg'
+  },
+  {
+    name: 'Erlich Bachman',
+    url: './img/erlich.jpg'
+  },
+  {
+    name: 'Monica Hall',
+    url: './img/monica.jpg'
+  },
+  {
+    name: 'Jared Dunn',
+    url: './img/jared.jpg'
+  },
+  {
+    name: 'Dinesh Chugtai',
+    url: './img/dinesh.jpg'
+  }
+]
+
+const alredyRemoved = []
+let charactersState = db // This fixes issues with updating characters state forcing it to use the current state and not the state that was active when the card was created.
+
+function Advanced () {
+  const [characters, setCharacters] = useState(db)
+  const [lastDirection, setLastDirection] = useState()
+
+  const childRefs = useMemo(() => Array(db.length).fill(0).map(i => React.createRef()), [])
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+    alredyRemoved.push(nameToDelete)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+    charactersState = charactersState.filter(character => character.name !== name)
+    setCharacters(charactersState)
+  }
+
+  const swipe = (dir) => {
+    const cardsLeft = characters.filter(person => !alredyRemoved.includes(person.name))
+    if (cardsLeft.length) {
+      const toBeRemoved = cardsLeft[cardsLeft.length - 1].name // Find the card object to be removed
+      const index = db.map(person => person.name).indexOf(toBeRemoved) // Find the index of which to make the reference to
+      alredyRemoved.push(toBeRemoved) // Make sure the next card gets removed next time if this card do not have time to exit the screen
+      childRefs[index].current.swipe(dir) // Swipe the card!
+    }
+  }
+
+  return (
+    <div>
+      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+      <h1>React Tinder Card</h1>
+      <div className='cardContainer'>
+        {characters.map((character, index) =>
+          <TinderCard ref={childRefs[index]} className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+              <h3>{character.name}</h3>
+            </div>
+          </TinderCard>
+        )}
+      </div>
+      <div className='buttons'>
+        <button onClick={() => swipe('left')}>Swipe left!</button>
+        <button onClick={() => swipe('right')}>Swipe right!</button>
+      </div>
+      {lastDirection ? <h2 key={lastDirection} className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText'>Swipe a card or press a button to get started!</h2>}
+    </div>
+  )
+}
+
+export default Advanced

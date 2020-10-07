e81d8094870f9e98973ae265d7b73f2f66c3e28c
Jakob <jakob@folkdatorn.se>
2020-10-07 22:16:58 +0200

    New gh-pages build

---
+++ docs/precache-manifest.84c34476c19452b88b2f603862ad07d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c118795656cd8ab984eea7a185c9a020",
+    "url": "/react-tinder-card-demo/index.html"
+  },
+  {
+    "revision": "9ba248091de8ecd82483",
+    "url": "/react-tinder-card-demo/static/css/main.45f55770.chunk.css"
+  },
+  {
+    "revision": "16a3db2cea06b4f7551a",
+    "url": "/react-tinder-card-demo/static/js/2.9a839f3d.chunk.js"
+  },
+  {
+    "revision": "9ba248091de8ecd82483",
+    "url": "/react-tinder-card-demo/static/js/main.04d692f5.chunk.js"
+  },
+  {
+    "revision": "0d6c761cd56427a1dae1",
+    "url": "/react-tinder-card-demo/static/js/runtime-main.aef4b6f8.js"
+  }
+]);

c79d8f969c2bd60c42426dfaf851db306a629b7e
Ben Miller <bsmiller25@gmail.com>
2021-02-15 13:36:34 -0500

    make simpler

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import React, { useState } from 'react'
 import './App.css'
 import Switch from 'react-ios-switch'
 
-import Advanced from './examples/Advanced'
 import Simple from './examples/Simple'
 
 function App () {
-  const [showAdvanced, setShowAdvanced] = useState(true)
 
   return (
     <div className='app'>
-      {showAdvanced ? <Advanced /> : <Simple />}
-      <div className='row'>
-        <p style={{ color: '#fff' }}>Show advanced example</p> <Switch checked={showAdvanced} onChange={setShowAdvanced} />
-      </div>
+      <Simple />
     </div>
   )
 }

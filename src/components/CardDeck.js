40e10ed9a35c21ea66843ba001dcb739e3fd7e12
Ben Miller <bsmiller25@gmail.com>
2021-02-15 17:13:15 -0500

    bugfix: set state to show modal for appropriate card

--- src/components/CardDeck.js
+++ src/components/CardDeck.js
@@ -19,23 +19,31 @@ function CardDeck () {
     console.log(name + ' left the screen!')
   }
 
-  const [showExpanded, setShowExpanded] = React.useState(false);
+    const [showExpanded, setShowExpanded] = React.useState(false);
+
+    const [profID, setProfID] = React.useState(0);
+
+    function clickMore(char){
+	setShowExpanded(true);
+	setProfID(char.id)
+    }
 
   return (
       <div>
       <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
       <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
       <h1>Tinderloin</h1>
+     
       <div className='cardContainer'>
       {characters.map((character) =>
 	  <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
 	  <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
 	  <h3>{character.name}</h3>
-	  <h4 onClick={() => setShowExpanded(true)}>See more</h4>
+	  <h4 onClick={() => clickMore(character)}>See more</h4>
 	  <ReactModal
 	  className="Modal"
 	  overlayClassName="Overlay"
-	  isOpen={showExpanded}
+	  isOpen={showExpanded && profID==character.id}
 	  contentLabel="example"
 	  >
 	  <img className="photo" src={character.url}/>

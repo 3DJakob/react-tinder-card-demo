ad487616c2092b6f790e30eea800e2bd9f8ab1bf
Luca Ziliani <lucagcc@gmail.com>
2021-10-26 07:55:24 +0200

    handle simultaneous goBack and outOfFrame events

--- src/examples/RestoreCard.js
+++ src/examples/RestoreCard.js
@@ -1,6 +1,6 @@
 import React, { useState, useMemo, useRef } from 'react'
-import TinderCard from '../react-tinder-card/index'
-//import TinderCard from 'react-tinder-card'
+//import TinderCard from '../react-tinder-card/index'
+import TinderCard from 'react-tinder-card'
 
 const db = [
   {
@@ -31,45 +31,46 @@ function RestoreCard () {
   //used for outOfFrame closure
   const currentIndexRef = useRef(currentIndex)
 
-  console.log("currentIndex", currentIndex)
-
   const childRefs = useMemo(
     () => Array(db.length).fill(0).map(i => React.createRef()), []
   )
 
-  const _setCurrentIndex = (val) => {
+  const updateCurrentIndex = (val) => {
     setCurrentIndex(val)
     currentIndexRef.current = val
   }
 
   const canGoBack = () => currentIndex < db.length - 1
   
-  const canSwipe = () => currentIndex > 0
+  const canSwipe = () => currentIndex >= 0
 
-
-  const swiped = (direction, nameToDelete) => {
-    console.log('hiding: ' + nameToDelete)
+  //set last direction and decrease current index
+  const swiped = (direction, nameToDelete, index) => {
     setLastDirection(direction)
-    _setCurrentIndex(currentIndex - 1)
+    updateCurrentIndex(index - 1)
   }
 
   const outOfFrame = (name, idx) => {
     console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current)
     //handle the case in which go back is pressed before card goes outOfFrame
-    currentIndexRef.current === idx && childRefs[idx].current.restoreCard()
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard()
+    // TODO: when quickly swipe and restore multiple times the same card, 
+    // it happens multiple outOfFrame events are queued and the card disappear 
+    // during latest swipes.Only the last outOfFrame event should be considered valid
   }
 
-  const swipe = (dir) => {
-    if (currentIndex>=0 && currentIndex<db.length) {
-      childRefs[currentIndex].current.swipe(dir) // Swipe the card!
+  const swipe = async (dir) => {
+    if (canSwipe() && currentIndex < db.length) {
+      await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
     }
   }
 
-  const goBack = () => {
+  //increase current index and show card
+  const goBack = async () => {
     if (!canGoBack()) return
     const newIndex = currentIndex + 1;
-    _setCurrentIndex(newIndex)
-    childRefs[newIndex].current.restoreCard()
+    updateCurrentIndex(newIndex)
+    await childRefs[newIndex].current.restoreCard()
   }
 
 
@@ -80,7 +81,7 @@ function RestoreCard () {
       <h1>React Tinder Card</h1>
       <div className='cardContainer'>
         {db.map((character, index) =>
-          <TinderCard ref={childRefs[index]} className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name, index)}>
+          <TinderCard ref={childRefs[index]} className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name, index)} onCardLeftScreen={() => outOfFrame(character.name, index)}>
             <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
               <h3>{character.name}</h3>
             </div>

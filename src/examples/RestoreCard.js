72215f2c73d1f83341af273f81d6300f75b8daa4
Jakob Unnebäck <jakob@folkdatorn.se>
2021-10-27 13:23:58 +0200

    Minor style changes and cleanup

--- src/examples/RestoreCard.js
+++ src/examples/RestoreCard.js
@@ -1,35 +1,35 @@
-import React, { useState, useMemo, useRef } from "react";
-//import TinderCard from '../react-tinder-card/index'
-import TinderCard from "react-tinder-card";
+import React, { useState, useMemo, useRef } from 'react'
+// import TinderCard from '../react-tinder-card/index'
+import TinderCard from 'react-tinder-card'
 
 const db = [
   {
-    name: "Richard Hendricks",
-    url: "./img/richard.jpg",
+    name: 'Richard Hendricks',
+    url: './img/richard.jpg'
   },
   {
-    name: "Erlich Bachman",
-    url: "./img/erlich.jpg",
+    name: 'Erlich Bachman',
+    url: './img/erlich.jpg'
   },
   {
-    name: "Monica Hall",
-    url: "./img/monica.jpg",
+    name: 'Monica Hall',
+    url: './img/monica.jpg'
   },
   {
-    name: "Jared Dunn",
-    url: "./img/jared.jpg",
+    name: 'Jared Dunn',
+    url: './img/jared.jpg'
   },
   {
-    name: "Dinesh Chugtai",
-    url: "./img/dinesh.jpg",
-  },
-];
+    name: 'Dinesh Chugtai',
+    url: './img/dinesh.jpg'
+  }
+]
 
-function RestoreCard() {
-  const [currentIndex, setCurrentIndex] = useState(db.length - 1);
-  const [lastDirection, setLastDirection] = useState();
-  //used for outOfFrame closure
-  const currentIndexRef = useRef(currentIndex);
+function RestoreCard () {
+  const [currentIndex, setCurrentIndex] = useState(db.length - 1)
+  const [lastDirection, setLastDirection] = useState()
+  // used for outOfFrame closure
+  const currentIndexRef = useRef(currentIndex)
 
   const childRefs = useMemo(
     () =>
@@ -37,95 +37,91 @@ function RestoreCard() {
         .fill(0)
         .map((i) => React.createRef()),
     []
-  );
+  )
 
   const updateCurrentIndex = (val) => {
-    setCurrentIndex(val);
-    currentIndexRef.current = val;
-  };
+    setCurrentIndex(val)
+    currentIndexRef.current = val
+  }
 
-  const canGoBack = () => currentIndex < db.length - 1;
+  const canGoBack = currentIndex < db.length - 1
 
-  const canSwipe = () => currentIndex >= 0;
+  const canSwipe = currentIndex >= 0
 
-  //set last direction and decrease current index
+  // set last direction and decrease current index
   const swiped = (direction, nameToDelete, index) => {
-    setLastDirection(direction);
-    updateCurrentIndex(index - 1);
-  };
+    setLastDirection(direction)
+    updateCurrentIndex(index - 1)
+  }
 
   const outOfFrame = (name, idx) => {
-    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
-    //handle the case in which go back is pressed before card goes outOfFrame
-    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current)
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard()
     // TODO: when quickly swipe and restore multiple times the same card,
     // it happens multiple outOfFrame events are queued and the card disappear
-    // during latest swipes.Only the last outOfFrame event should be considered valid
-  };
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  }
 
   const swipe = async (dir) => {
-    if (canSwipe() && currentIndex < db.length) {
-      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    if (canSwipe && currentIndex < db.length) {
+      await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
     }
-  };
+  }
 
-  //increase current index and show card
+  // increase current index and show card
   const goBack = async () => {
-    if (!canGoBack()) return;
-    const newIndex = currentIndex + 1;
-    updateCurrentIndex(newIndex);
-    await childRefs[newIndex].current.restoreCard();
-  };
+    if (!canGoBack) return
+    const newIndex = currentIndex + 1
+    updateCurrentIndex(newIndex)
+    await childRefs[newIndex].current.restoreCard()
+  }
 
   return (
     <div>
       <link
-        href="https://fonts.googleapis.com/css?family=Damion&display=swap"
-        rel="stylesheet"
+        href='https://fonts.googleapis.com/css?family=Damion&display=swap'
+        rel='stylesheet'
       />
       <link
-        href="https://fonts.googleapis.com/css?family=Alatsi&display=swap"
-        rel="stylesheet"
+        href='https://fonts.googleapis.com/css?family=Alatsi&display=swap'
+        rel='stylesheet'
       />
       <h1>React Tinder Card</h1>
-      <div className="cardContainer">
+      <div className='cardContainer'>
         {db.map((character, index) => (
           <TinderCard
             ref={childRefs[index]}
-            className="swipe"
+            className='swipe'
             key={character.name}
             onSwipe={(dir) => swiped(dir, character.name, index)}
             onCardLeftScreen={() => outOfFrame(character.name, index)}
           >
             <div
-              style={{ backgroundImage: "url(" + character.url + ")" }}
-              className="card"
+              style={{ backgroundImage: 'url(' + character.url + ')' }}
+              className='card'
             >
               <h3>{character.name}</h3>
             </div>
           </TinderCard>
         ))}
       </div>
-      <div className="buttons">
-        {canSwipe() && (
-          <button onClick={() => swipe("left")}>Swipe left!</button>
-        )}
-        {canSwipe() && (
-          <button onClick={() => swipe("right")}>Swipe right!</button>
-        )}
-        {canGoBack() && <button onClick={() => goBack()}>Go Back!</button>}
+      <div className='buttons'>
+        <button style={{ backgroundColor: !canSwipe && '#c3c4d3' }} onClick={() => swipe('left')}>Swipe left!</button>
+        <button style={{ backgroundColor: !canGoBack && '#c3c4d3' }} onClick={() => goBack()}>Undo swipe!</button>
+        <button style={{ backgroundColor: !canSwipe && '#c3c4d3' }} onClick={() => swipe('right')}>Swipe right!</button>
       </div>
       {lastDirection ? (
-        <h2 key={lastDirection} className="infoText">
+        <h2 key={lastDirection} className='infoText'>
           You swiped {lastDirection}
         </h2>
       ) : (
-        <h2 className="infoText">
+        <h2 className='infoText'>
           Swipe a card or press a button to get Restore Card button visible!
         </h2>
       )}
     </div>
-  );
+  )
 }
 
-export default RestoreCard;
+export default RestoreCard
